0245ab93e12d96a3f781a7bcf7d3569a97934613
volkoffff <robin.lanfranchi@etudiant.univ-reims.fr>
2023-09-29 09:34:02 +0200

    feat: Home page

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@ import { RouterLink, RouterView } from 'vue-router'
   <header>
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/"></RouterLink>
       </nav>
   </header>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,18 @@
 <script setup>
 
+import { RouterLink, RouterView } from 'vue-router'
+import moviesHome from '../components/icons/moviesHome.vue';
+import actorsHome from '../components/icons/actorsHome.vue';
+
 </script>
 
 <template>
 
-  
+    <h1>
+      Bienvenu sur la page d'accueil !
+    </h1>
+    <moviesHome />
+    <actorsHome />
+
 
 </template>
