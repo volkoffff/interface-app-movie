d406015b8881e98e2e352b751a2a3a9d5df7a3bb
volkoffff <robin.lanfranchi@etudiant.univ-reims.fr>
2024-01-23 15:15:16 +0100

    style(actor): add style to actor card

--- src/components/actorsCard.vue
+++ src/components/actorsCard.vue
@@ -14,7 +14,32 @@ defineProps({
 
 <template>
     <router-link :to="`/actor/${actor.id}`">
-        {{ actor.firstName }} {{ actor.lastName }}
-        {{ actor.nationalite.nationalite }}
-    </router-link>
+        <div class="actor-card">
+            <div class="actor-image" style="background-image: url(https://castprod.com/wp-content/uploads/2022/06/jean-dujardin.jpg);"></div>
+            <p class="card-title">
+                {{ actor.firstName }} {{ actor.lastName }}
+            </p>
+            <p class="card-subtitle">
+                {{ actor.nationalite.nationalite }}
+            </p>
+        </div>
+        </router-link>
 </template>
+
+<style>
+.actor-card {
+    display: flex;
+    flex-direction: column;
+    gap: 3px;
+    width: 120px;
+    overflow: hidden;
+}
+
+.actor-image {
+    border-radius: 8px;
+    aspect-ratio: 3/4;
+    background-position: center;
+    background-size: cover;
+    width: 120px;
+}
+</style>
