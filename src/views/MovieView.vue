587672540c23dabfc7a67c257deff852f808d036
volkoffff <robin.lanfranchi@etudiant.univ-reims.fr>
2024-01-23 15:15:55 +0100

    feat(movie): add etition for a movie and format date

--- src/views/MovieView.vue
+++ src/views/MovieView.vue
@@ -1,8 +1,9 @@
 <script setup>
 import moviesCard from '../components/moviesCard.vue';
 import { useRoute, RouterLink, RouterView } from 'vue-router';
-import { onMounted, ref, onBeforeUnmount } from 'vue';
+import { onMounted, ref, onBeforeUnmount, reactive } from 'vue';
 import axios from 'axios';
+import moment from 'moment'
 
 const route = useRoute();
 const routeId = route.params.id
@@ -10,14 +11,32 @@ const routeId = route.params.id
 const data = ref('');
 const data2 = ref('');
 
-    console.log(routeId);
-onMounted(async () => {
+const editedMovieTitle = ref('')
+const editedMovieDescription = ref('')
+const editedMovieDate = ref('')
+
+const errorModificationJson = ref(null)
+
+onMounted(() => {
+    load();
+});
+
+const load = async() => {
+
     const response = await axios.get(`http://127.0.0.1:8000/api/movies/${routeId}`, {
         headers: {
             Authorization: `Bearer ${localStorage.getItem('authToken')}`,
         },
     });
-    data.value = response.data;
+    data.value = response.data; 
+
+    //attribution for the modal 
+    editedMovieTitle.value = data.value.title;
+    editedMovieDescription.value = data.value.description;
+    console.log(data.value.releaseDate);
+    editedMovieDate.value = data.value.releaseDate.split('T')[0];
+    console.log(data.value)
+
     const cathegory = ref(data.value.category.id);
     const response2 = await axios.get(`http://127.0.0.1:8000/api/categories/${cathegory.value}`, {
         headers: {
@@ -48,10 +67,57 @@ onMounted(async () => {
     }
 
     // Appel de la fonction pour l'activer
-    addActiveClassOnScroll();
+    addActiveClassOnScroll();    
 
-});
+}
 
+
+
+
+async function updateMovieTitle() {
+    console.log(data.value);
+    if (ModifyState) {
+        try {
+          const token = localStorage.getItem('authToken'); // Récupérer le token d'authentification
+          if (!token) {
+            //--- rediriger l'utilisateur vers la page de connexion
+            this.$router.push('/');
+            return;
+          }
+          const headers = {
+            Authorization: `Bearer ${token}`,
+            'Content-Type': 'application/merge-patch+json',
+          };
+
+          const updatedMovie = reactive({ title: editedMovieTitle.value, description: editedMovieDescription.value, releaseDate: editedMovieDate.value }); // Nouveau titre du film
+
+          // Envoyer la requête PATCH à l'API pour mettre à jour le titre du film
+          await axios.patch(`http://127.0.0.1:8000/api/movies/${data.value.id}`, updatedMovie, { headers });
+
+          // Réinitialiser après la mise à jour
+          editedMovieTitle.value = '';
+          editedMovieDescription.value = '';
+          editedMovieDate.value = '';
+
+          
+          // Réinitialiser la sélection du film après modification
+          ModifyState.value = false;
+          
+            // Rafraîchir la liste des films
+            load();
+            errorModificationJson.value = null
+
+        } catch (error) {
+            // Probleme validation des données
+            if (error.response.data.status == 422) {
+
+                errorModificationJson.value = textToJson(error.response.data.detail);
+                console.log('erreur sur:',errorModificationJson.value);
+            }
+          console.error('Erreur lors de la mise à jour du du film :', error.response.data);
+        }
+      }
+}
 
 //scroll
 // Fonction pour mettre à jour la taille du background en fonction du défilement
@@ -61,7 +127,7 @@ const updateBackgroundSize = () => {
     if (topMovie) {
         const scrollPosition = window.scrollY;
         // Modifiez la taille du background en fonction de la position de défilement
-        backgroundSize = 120 - scrollPosition * 0.05; // Vous pouvez ajuster le coefficient selon vos besoins
+        backgroundSize = 120 - scrollPosition * 0.08; // Vous pouvez ajuster le coefficient selon vos besoins
         topMovie.style.backgroundSize = `${backgroundSize}%`;
     }
 };
@@ -75,12 +141,44 @@ onBeforeUnmount(() => {
 });
 
 
+// modal to modify setup
+const ModifyState = ref(false)
+console.log(ModifyState);
+function toggleModifyTrue() {
+    ModifyState.value = true
+    console.log(ModifyState.value);
+}
+function toggleModifyFalse() {
+    ModifyState.value = false
+    console.log(ModifyState.value);
+}
+
+
+function textToJson(inputText) {
+  const lines = inputText.split('\n');
+  const result = {};
+
+  lines.forEach(line => {
+    // Utiliser une expression régulière pour extraire la clé et la valeur
+    const match = line.match(/([^:]+): (.+)/);
+
+    if (match) {
+      const key = match[1].trim();
+      const value = match[2].trim();
+      result[key] = value;
+    }
+  });
+
+  return result;
+}
+
+
 </script>
 
 
 <template>
 
-    <div v-if="data">
+<div v-if="data">
 
         <div class="banner-movie">
             <div class="banner-movie-left">
@@ -112,6 +210,8 @@ onBeforeUnmount(() => {
                             </RouterLink>
                             <p>•</p>
                             <p>{{ data.duration }} minutes</p>
+                            <p>•</p>
+                            <p>{{ moment(data.releaseDate).format('LL') }}</p>
                         </div>
                     </div>
                     <div class="mini-section">
@@ -136,10 +236,10 @@ onBeforeUnmount(() => {
                     </div>
                     <div class="jc">
                         <button class="translucide-btn"><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M4 6H2v14c0 1.1.9 2 2 2h14v-2H4V6zm16-4H8c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm-8 12.5v-9l6 4.5-6 4.5z"></path></svg>Bande annonce</button>
-                        <RouterLink :to="`/movie/edit/${data.id}`" :data="data" class="translucide-main-btn">
+                        <div @click="toggleModifyTrue()" class="translucide-main-btn">
                             <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"></path></svg>
                             Modifier
-                        </RouterLink>
+                        </div>
                     </div>
                 </div> <!-- top-movie-bottom-left -->
 
@@ -160,7 +260,128 @@ onBeforeUnmount(() => {
             
         
     
+        <div :class="['bg-modal', { 'hidden': !ModifyState }]">
+
+        <div class="modal">
+            <div class="modal-top"><div @click="toggleModifyFalse()"><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M563.8 512l262.5-312.9c4.4-5.2.7-13.1-6.1-13.1h-79.8c-4.7 0-9.2 2.1-12.3 5.7L511.6 449.8 295.1 191.7c-3-3.6-7.5-5.7-12.3-5.7H203c-6.8 0-10.5 7.9-6.1 13.1L459.4 512 196.9 824.9A7.95 7.95 0 0 0 203 838h79.8c4.7 0 9.2-2.1 12.3-5.7l216.5-258.1 216.5 258.1c3 3.6 7.5 5.7 12.3 5.7h79.8c6.8 0 10.5-7.9 6.1-13.1L563.8 512z"></path></svg></div></div>
+            <h2>Modification d'un Film</h2>
+            <form @submit.prevent="updateMovieTitle" class="form-modal">
+                <div class="form-group">
+                    <label for="editMovieTitle">Nom du film</label>
+                    <input
+                    :class="[{ 'error-border': errorModificationJson && errorModificationJson.title }]"
+                    type="text"
+                    class="form-control"
+                    id="editMovieTitle"
+                    v-model="editedMovieTitle"
+                    :placeholder="data.title"
+                    />
+
+                    <div class="error-text" v-if="errorModificationJson && errorModificationJson.title" >• {{ errorModificationJson.title }}</div>
+                </div>
+
+                <div class="form-group">
+                    <label for="editMovieDescription">Description</label>
+                    <input
+                    :class="[{ 'error-border': errorModificationJson && errorModificationJson.description }]"
+                    type="text"
+                    class="form-control"
+                    id="editMovieDescription"
+                    v-model="editedMovieDescription"
+                    :placeholder="data.description"
+                    />
+
+                    <div class="error-text" v-if="errorModificationJson && errorModificationJson.description" >• {{ errorModificationJson.description }}</div>
+                </div>
+
+                <div class="form-group">
+                    <label for="editMovieDate">Date de parution</label>
+                    <input
+                    :class="[{ 'error-border': errorModificationJson && errorModificationJson.releaseDate }]"
+                    type="date"
+                    class="form-control"
+                    id="editMovieDate"
+                    v-model="editedMovieDate"
+                    :placeholder="data.date"
+                    />
+
+                    <div class="error-text" v-if="errorModificationJson && errorModificationJson.releaseDate" >• {{ errorModificationJson.releaseDate }}</div>
+                </div>
+                
+                <div class="container-btn">
+                    <button type="submit" class="btn main-btn">Modifier</button>
+                </div>
+            </form>
+        </div>
+        
+        
     </div>
 
 
+</div>
+
+<div v-else>
+    <div class="loading-top-movie">
+        <div class="loading-center">
+            <div class="loading-image loading"></div>
+            <div class="loading-text">
+                <div>
+                    <p class="loading-head-line loading"></p>
+                    <p class="loading-line-500 loading"></p>
+                </div>
+                <p class="loading-line-700-80 loading"></p>
+                <p class="loading-line-200 loading"></p>
+            </div>
+        </div>
+    </div>
+</div>
+
+
 </template>
+
+<style>
+
+.loading-top-movie {
+    height: 100vh;
+    width: 100%;
+    display: flex;
+    align-items: center;
+}
+.loading-center {
+    gap: 40px;
+    padding: 40px;
+    display: flex;
+    align-items: flex-start;
+}
+.loading-image {
+    height: 500px;
+    aspect-ratio: 3/4;
+}
+.loading-text {
+    padding: 10px 0;
+    display: flex;
+    flex-direction: column;
+    gap: 40px;
+}
+.loading-text > div {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+}
+.loading-head-line {
+    height: 50px ;
+    width: 300px;
+}
+.loading-line-200 {
+    height: 30px;
+    width: 200px;
+}
+.loading-line-500 {
+    height: 30px;
+    width: 500px;
+}
+.loading-line-700-80 {
+    height: 80px;
+    width: 700px;
+}
+</style>
