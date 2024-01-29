54b5b15ff27b579e82bb1cfa48f429879d92cb47
volkoffff <robin.lanfranchi@etudiant.univ-reims.fr>
2024-01-29 14:58:23 +0100

    fix(navbar): minor change

--- src/components/searchbarNavbar.vue
+++ src/components/searchbarNavbar.vue
@@ -40,7 +40,7 @@ window.addEventListener('scroll', removeFocusOnScroll);
 <template>
   <div class="search">
     <svg class="search-icon" stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>
-    <input @input="fetchData" v-model="searchQuery" class="searchbar-navbar" placeholder="Search a movie" type="text">
+    <input @input="fetchData" v-model="searchQuery" class="searchbar-navbar" placeholder="Rechercher un film" type="text">
     <div class="searchbar-navbar-result">
       <div v-for="(movie, index) in data" :key="movie.id">
         <div class="movie-card">
