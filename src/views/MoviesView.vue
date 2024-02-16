e7233210eac65fbbd5a4f868c418bdb071f5e34f
volkoffff <robin.lanfranchi@etudiant.univ-reims.fr>
2024-02-16 14:33:25 +0100

    feat(movies): add arrow to next and previous pages buttons

--- src/views/MoviesView.vue
+++ src/views/MoviesView.vue
@@ -1,20 +1,22 @@
 <script setup>
-import moviesCard from '../components/moviesCard.vue';
-import { onMounted, ref, computed } from 'vue'
-import axios from 'axios'
+import moviesCard from "../components/moviesCard.vue";
+import { onMounted, ref, computed } from "vue";
+import axios from "axios";
 
 const data = ref([]);
 const dataSaved = ref([]);
 const dataCategorie = ref([]);
 const itemsPerPage = 15; // Nombre d'éléments par page
 const currentPage = ref(1);
-const searchQuery = ref('');
-const selectedCategory = ref(''); // Catégorie sélectionnée pour le filtre
+const searchQuery = ref("");
+const selectedCategory = ref(""); // Catégorie sélectionnée pour le filtre
 
 const filterByCategory = () => {
   if (selectedCategory.value) {
     // Filtrer les films par catégorie
-    data.value = dataSaved.value.filter(movie => movie.category.id === selectedCategory.value);
+    data.value = dataSaved.value.filter(
+      (movie) => movie.category.id === selectedCategory.value
+    );
   } else {
     // Afficher tous les films si aucune catégorie n'est sélectionnée
     data.value = dataSaved.value;
@@ -24,7 +26,7 @@ const filterByCategory = () => {
 onMounted(async () => {
   await fetchData();
   await fetchDataCategorie();
-})
+});
 
 const displayedData = computed(() => {
   const startIndex = (currentPage.value - 1) * itemsPerPage;
@@ -35,12 +37,12 @@ const displayedData = computed(() => {
 const totalPages = computed(() => Math.ceil(data.value.length / itemsPerPage));
 
 const fetchData = async () => {
-  const response = await axios.get('http://127.0.0.1:8000/api/movies', {
+  const response = await axios.get("http://127.0.0.1:8000/api/movies", {
     params: {
       title: searchQuery.value, // Utilisez la valeur de recherche dans la requête
     },
     headers: {
-      Authorization: `Bearer ${localStorage.getItem('authToken')}`,
+      Authorization: `Bearer ${localStorage.getItem("authToken")}`,
     },
   });
   data.value = response.data["hydra:member"];
@@ -48,13 +50,13 @@ const fetchData = async () => {
 };
 
 const fetchDataCategorie = async () => {
-  const response2 = await axios.get('http://127.0.0.1:8000/api/categories', {
+  const response2 = await axios.get("http://127.0.0.1:8000/api/categories", {
     headers: {
-      Authorization: `Bearer ${localStorage.getItem('authToken')}`,
+      Authorization: `Bearer ${localStorage.getItem("authToken")}`,
     },
   });
   dataCategorie.value = response2.data["hydra:member"];
-}
+};
 
 const nextPage = () => {
   if (currentPage.value < totalPages.value) {
@@ -67,43 +69,113 @@ const previousPage = () => {
     currentPage.value--;
   }
 };
-
-
-
 </script>
 
-
-
 <template>
-  <div style="padding-top: 5rem;">
-
-
-
+  <div style="padding-top: 5rem">
     <div class="movies-list" v-if="data">
       <h3>Liste de touts les films</h3>
       <form class="search">
-        <svg class="search-icon" stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24"
-          stroke-linecap="round" stroke-linejoin="round" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+        <svg
+          class="search-icon"
+          stroke="currentColor"
+          fill="none"
+          stroke-width="2"
+          viewBox="0 0 24 24"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          height="1em"
+          width="1em"
+          xmlns="http://www.w3.org/2000/svg"
+        >
           <circle cx="11" cy="11" r="8"></circle>
           <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
         </svg>
-        
-        <input @input="fetchData" class="searchbar" type="text" v-model="searchQuery" placeholder="Rechercher un film" />
-        <select id="category"  class="select" v-model="selectedCategory" @change="filterByCategory">
+
+        <input
+          @input="fetchData"
+          class="searchbar"
+          type="text"
+          v-model="searchQuery"
+          placeholder="Rechercher un film"
+        />
+        <select
+          id="category"
+          class="select"
+          v-model="selectedCategory"
+          @change="filterByCategory"
+        >
           <option value="">Toutes les catégories</option>
-          <option v-for="category in dataCategorie" :key="category.id" :value="category.id">{{ category.name }}</option>
+          <option
+            v-for="category in dataCategorie"
+            :key="category.id"
+            :value="category.id"
+          >
+            {{ category.name }}
+          </option>
         </select>
       </form>
       <div v-for="(movie, index) in displayedData" :key="movie.id">
         <moviesCard :movie="movie" />
       </div>
       <div class="page-selector-section">
-        <button class="main-btn" @click="previousPage" :disabled="currentPage === 1">Page précédente</button>
+        <button
+          class="main-btn"
+          @click="previousPage"
+          :disabled="currentPage === 1"
+        >
+          <svg
+            stroke="currentColor"
+            fill="currentColor"
+            stroke-width="0"
+            viewBox="0 0 16 16"
+            height="1em"
+            width="1em"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M5.854 4.646a.5.5 0 010 .708L3.207 8l2.647 2.646a.5.5 0 01-.708.708l-3-3a.5.5 0 010-.708l3-3a.5.5 0 01.708 0z"
+              clip-rule="evenodd"
+            ></path>
+            <path
+              fill-rule="evenodd"
+              d="M2.5 8a.5.5 0 01.5-.5h10.5a.5.5 0 010 1H3a.5.5 0 01-.5-.5z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+          Page précédente
+        </button>
         <span>Page {{ currentPage }}</span>
-        <button class="main-btn" @click="nextPage" :disabled="currentPage === totalPages">Page suivante</button>
+        <button
+          class="main-btn"
+          @click="nextPage"
+          :disabled="currentPage === totalPages"
+        >
+          Page suivante
+          <svg
+            stroke="currentColor"
+            fill="currentColor"
+            stroke-width="0"
+            viewBox="0 0 16 16"
+            height="1em"
+            width="1em"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M10.146 4.646a.5.5 0 01.708 0l3 3a.5.5 0 010 .708l-3 3a.5.5 0 01-.708-.708L12.793 8l-2.647-2.646a.5.5 0 010-.708z"
+              clip-rule="evenodd"
+            ></path>
+            <path
+              fill-rule="evenodd"
+              d="M2 8a.5.5 0 01.5-.5H13a.5.5 0 010 1H2.5A.5.5 0 012 8z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+        </button>
       </div>
     </div>
-
   </div>
 </template>
 
@@ -133,7 +205,7 @@ form {
 }
 .main-btn:disabled {
   opacity: 0.4;
-  pointer-events: none  ;
+  pointer-events: none;
 }
 .search {
   position: relative;
@@ -149,7 +221,7 @@ form {
   z-index: 1;
 }
 
-.search-icon>* {
+.search-icon > * {
   color: rgb(176, 176, 176);
 }
 
